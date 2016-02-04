24fd40f272ff39fa31687863df7ec6ae0d037130
Salvador de la Puente González <salva@unoyunodiez.com>
2016-02-04 12:10:48 +0100

    Testing web available and not timing out

--- tests/service-worker/testGet.js
+++ tests/service-worker/testGet.js
@@ -8,27 +8,26 @@ describe('get()', function() {
   'use strict';
 
   var clock;
+  var fakeCache;
 
   beforeEach(function() {
+    fakeCache = {
+      match: sinon.stub().returns(Promise.resolve(new Response('match'))),
+      put: sinon.stub().returns(Promise.resolve())
+    };
     clock = sinon.useFakeTimers();
     importScripts('/base/wp-offline/lib/js/sw.js');
+    sinon.stub(wpOffline, 'openCache').returns(Promise.resolve(fakeCache));
+    sinon.stub(Response.prototype, 'clone').returnsThis();
   });
 
   afterEach(function() {
     clock.restore();
+    Response.prototype.clone.restore();
   });
 
-  function addByPassWhenNetwork() {
+  function addByPassWhenNetwork(networkResponse) {
     describe('get() when network available but request is not a GET or url is excluded', function() {
-      var networkResponse = new Response();
-
-      before(function() {
-        sinon.stub(self, 'fetch').returns(Promise.resolve(networkResponse));
-      });
-
-      after(function() {
-        self.fetch.restore();
-      });
 
       it('always fetches from network if excluded', function() {
         sinon.stub(wpOffline, 'isExcluded').returns(true);
@@ -53,7 +52,7 @@ describe('get()', function() {
   }
 
   function addByPassWhenNoNetwork() {
-    describe('get() when network non available and request is not a GET or url is excluded', function() {
+    describe('get() when network not available and request is not a GET or url is excluded', function() {
       var networkError = {};
 
       before(function() {
@@ -86,23 +85,50 @@ describe('get()', function() {
     });
   }
 
+  // TODO: It remains to check clone(). This can bit us.
   describe('get() when network is available and it does not time out', function() {
+    var networkResponse = new Response('success!');
 
-    addByPassWhenNetwork();
+    before(function() {
+      sinon.stub(self, 'fetch').returns(Promise.resolve(networkResponse));
+    });
+
+    after(function() {
+      self.fetch.restore();
+    });
+
+    addByPassWhenNetwork(networkResponse);
 
     it('fetches from network', function() {
-
+      return wpOffline.get(new Request('test/url'))
+        .then(response => {
+          assert.strictEqual(response, networkResponse);
+        });
     });
 
     it('stores a fresh copy in the cache', function() {
-
+      var request = new Request('some/url');
+      return wpOffline.get(request)
+        .then(() => {
+          assert.isOk(fakeCache.put.calledOnce);
+          assert.isOk(fakeCache.put.calledWith(request, networkResponse));
+        });
     });
 
   });
 
   describe('get() when network is available but times out', function() {
+    var networkResponse = new Response('success!');
 
-    addByPassWhenNoNetwork();
+    before(function() {
+      sinon.stub(self, 'fetch').returns(Promise.resolve(networkResponse));
+    });
+
+    after(function() {
+      self.fetch.restore();
+    });
+
+    addByPassWhenNetwork(networkResponse);
 
     it('fetches from cache', function() {
 
