4085007a74bccc5e6af5ecaa7626821a538bbb40
Salvador de la Puente González <salva@unoyunodiez.com>
2016-02-19 13:07:22 +0100

    Tests for fetch

---
+++ tests/service-worker/testFetch.js
@@ -0,0 +1,29 @@
+describe('onFetch()', function() {
+  'use strict';
+
+  var fakeEvent;
+  var fakeResponse = {};
+
+  beforeEach(function() {
+    fakeEvent = {
+      request: {},
+      respondWith: sinon.stub()
+    };
+    importScripts('/base/wp-offline-content/lib/js/sw.js');
+    wpOfflineContent.get = sinon.stub().returns(fakeResponse);
+  });
+
+
+  it('do not respond if excluded', function() {
+    sinon.stub(wpOfflineContent, 'shouldBeHandled').returns(false);
+    wpOfflineContent.onFetch(fakeEvent);
+    assert.isFalse(fakeEvent.respondWith.called);
+  });
+
+  it('respond if not excluded', function() {
+    sinon.stub(wpOfflineContent, 'shouldBeHandled').returns(true);
+    wpOfflineContent.onFetch(fakeEvent);
+    assert.isTrue(fakeEvent.respondWith.calledOnce);
+    assert.isTrue(fakeEvent.respondWith.calledWith(fakeResponse));
+  });
+});
